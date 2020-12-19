a602ae76cbbe92176d949941a4de464b8029cafb
Anton Herzog <info@antonherzog.com>
2020-12-19 15:48:28 +0100

    Solve part one of day one.

--- src/Day01/Program.fs
+++ src/Day01/Program.fs
@@ -2,16 +2,29 @@
 
 open System
 
+let rec pairs l =
+    match l with
+    | [] | [_] -> []
+    | h :: t ->
+        [for x in t do
+            yield h,x
+         yield! pairs t]
+
 let splitAtLinebreak (s : string) =
     s.Split([|'\n'|])
 
-let integerInput =
+let partOne =
     input 
     |> splitAtLinebreak 
-    |> Array.map (int) 
+    |> Array.map (int)
+    |> List.ofArray
+    |> pairs
+    |> List.map (fun (a,b) -> (a,b), a + b)
+    |> List.filter (fun (_, sum) -> sum = 2020)
+    |> List.map (fun ((a,b), _) -> a * b)
 
 [<EntryPoint>]
 let main argv =
 
-    printfn "%A" "result"
+    printfn "%A" (partOne)
     0 // return an integer exit code
