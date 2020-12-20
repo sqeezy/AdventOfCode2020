948e886b5c142a4d23ca3cf10c0bc3bf740397d6
Anton Herzog <info@antonherzog.com>
2020-12-20 20:01:54 +0100

    Day two part two.

--- src/Day02/Day02/Program.fs
+++ src/Day02/Day02/Program.fs
@@ -1,12 +1,11 @@
-﻿open System
-open Day02.Input
+﻿open Day02.Input
 open FParsec
 
 let splitAtLinebreak (s : string) =
     s.Split([|'\n'|])
 let inputLines = input |> splitAtLinebreak
 
-type Entry = {Min : int; Max : int; Letter : char; Pw : string}
+type Entry = {A : int; B : int; Letter : char; Pw : string}
 
 let test p str =
     match run p str with
@@ -17,23 +16,40 @@ let lineParser = pipe4 pint32
                         (skipString "-" >>.pint32)
                         (spaces >>. asciiLower .>> skipString ":" .>> spaces)
                         (manyChars asciiLower)
-                        (fun min max c pw -> {Min=min; Max=max; Letter=c; Pw=pw})
+                        (fun min max c pw -> {A=min; B=max; Letter=c; Pw=pw})
 
-let validate {Min=min; Max=max; Letter=c; Pw=pw} =
+let validatePartOne {A=min; B=max; Letter=c; Pw=pw} =
     let matches = pw.ToCharArray() |> Array.filter (fun ch -> c = ch) |> Array.length
     min <= matches && matches <= max
 
-let partOne = inputLines
-              |> Array.map (run lineParser)
-              |> Array.choose (function
-                 | Success(entry,_,_) -> Some entry
-                 | _ -> None )
-              |> Array.filter validate
-              |> Array.length
+let validatePartTwo {A=index1; B=index2; Letter=c; Pw=pw} =
+    let letters = pw.ToCharArray()
+    let isMatch index =  letters |> Array.tryItem (index - 1) |> Option.map ((=) c)
+    [|index1; index2|]
+    |> Array.map isMatch
+    |> Array.choose (function Some true -> Some true | _ -> None)
+    |> Array.length
+    |> (=) 1
 
-test lineParser "3-7 f: fffffftfffhfff"
+
+
+
+let solve validate = Array.map (run lineParser)
+                     >> Array.choose (
+                        function
+                        | Success(entry,_,_) -> Some entry
+                        | _ -> None )
+                     >> Array.filter validate
+                     >> Array.length
+
+let partOneSolve = solve validatePartOne
+let partOne = inputLines |> partOneSolve
+
+let partTwoSolve = solve validatePartTwo
+let partTwo = inputLines |> partTwoSolve
 
 [<EntryPoint>]
 let main argv =
-    printf $"{partOne}"
+    printfn $"Part One:\t{partOne}"
+    printfn $"Part Two:\t{partTwo}"
     0
