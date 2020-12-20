03cc991ddf33e92e5f82a7bd5143d4cf827d2b78
Anton Herzog <info@antonherzog.com>
2020-12-20 19:04:22 +0100

    Day two part one.

--- src/Day02/Day02/Program.fs
+++ src/Day02/Day02/Program.fs
@@ -1,13 +1,39 @@
-﻿// Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
+﻿open System
+open Day02.Input
+open FParsec
 
-open System
+let splitAtLinebreak (s : string) =
+    s.Split([|'\n'|])
+let inputLines = input |> splitAtLinebreak
 
-// Define a function to construct a message to print
-let from whom =
-    sprintf "from %s" whom
+type Entry = {Min : int; Max : int; Letter : char; Pw : string}
+
+let test p str =
+    match run p str with
+    | Success(result, _, _)   -> printfn "Success: %A" result
+    | Failure(errorMsg, _, _) -> printfn "Failure: %s" errorMsg
+
+let lineParser = pipe4 pint32
+                        (skipString "-" >>.pint32)
+                        (spaces >>. asciiLower .>> skipString ":" .>> spaces)
+                        (manyChars asciiLower)
+                        (fun min max c pw -> {Min=min; Max=max; Letter=c; Pw=pw})
+
+let validate {Min=min; Max=max; Letter=c; Pw=pw} =
+    let matches = pw.ToCharArray() |> Array.filter (fun ch -> c = ch) |> Array.length
+    min <= matches && matches <= max
+
+let partOne = inputLines
+              |> Array.map (run lineParser)
+              |> Array.choose (function
+                 | Success(entry,_,_) -> Some entry
+                 | _ -> None )
+              |> Array.filter validate
+              |> Array.length
+
+test lineParser "3-7 f: fffffftfffhfff"
 
 [<EntryPoint>]
 let main argv =
-    let message = from "F#" // Call the function
-    printfn "Hello world %s" message
-    0 // return an integer exit code
+    printf $"{partOne}"
+    0
